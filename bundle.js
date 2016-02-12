486ddde20d14f2e5638f751bcd6f750495ccaa99
Thorsten Lorenz <thlorenz@gmx.de>
2016-02-12 18:02:01 -0500

    app: adding inject script feature

--- bundle.js
+++ bundle.js
@@ -1,12 +1,24 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+'use strict'
 
-var query = require('./query');
-var q = query.parse();
+var query = require('./query')
+var q = query.parse()
 
-console.dir(q);
-var message = document.getElementById('message');
-message.innerHTML = q.m || q.msg || q.message || 'Do whatever you want!';
+var message = document.getElementById('message')
+
+var msg = q.m || q.msg || q.message
+if (msg) {
+  message.innerHTML = msg
+}
+
+var script = q.s || q.script
+if (script) {
+  var el = document.createElement( 'script' )
+  el.type = 'text/javascript';
+  el.innerText = script
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(el, s);
+}
 
 },{"./query":5}],2:[function(require,module,exports){
 // Copyright Joyent, Inc. and other Node contributors.
--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-'use strict';
+'use strict'
 
-var query = require('./query');
-var q = query.parse();
+var query = require('./query')
+var q = query.parse()
 
-console.dir(q);
-var message = document.getElementById('message');
-message.innerHTML = q.m || q.msg || q.message || 'Do whatever you want!';
+var message = document.getElementById('message')
+
+var msg = q.m || q.msg || q.message
+if (msg) {
+  message.innerHTML = msg
+}
+
+var script = q.s || q.script
+if (script) {
+  var el = document.createElement( 'script' )
+  el.type = 'text/javascript';
+  el.innerText = script
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(el, s);
+}
