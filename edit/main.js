70aae32d5453ab4783a48d8364f297824873989d
Thorsten Lorenz <thlorenz@gmx.de>
2018-01-15 15:16:18 -0500

    edit: rendering editor + update

---
+++ edit/main.js
@@ -0,0 +1,45 @@
+/* global document */
+
+const ace = require('brace')
+require('brace/mode/html')
+require('brace/theme/monokai')
+
+function render() {
+  const existing = document.getElementById('editor')
+  if (existing != null) return
+
+  const html = `
+<div id="wrapper">
+  <div id="editor" class="editor"></div>
+  <div id="content" class="content"></div>
+</div
+`
+  document.body.innerHTML = html
+}
+
+render()
+
+const startingContent = `
+<h4>Edit Instructions</h4>
+<div>
+  Add any html or text here and see it <i>pre</i>rendered
+  on the right.
+  Then click the share button to obtain the URL to share
+  the instructions with others.
+</div>
+`
+
+const editor = ace.edit('editor')
+editor.getSession().setMode('ace/mode/html')
+editor.setTheme('ace/theme/monokai')
+editor.setValue(startingContent)
+
+function renderEdits() {
+  const html = editor.getValue()
+  const content = document.getElementById('content')
+  content.innerHTML = html
+}
+
+renderEdits()
+editor.on('change', renderEdits)
+
